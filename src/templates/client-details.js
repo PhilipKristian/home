30b2136466ce373e69826eee0286ab59a8606f37
Vincent Serpoul <vincent@serpoul.com>
2021-05-05 15:19:03 +0800

    fix windows stuff

--- src/templates/client-details.js
+++ src/templates/client-details.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { graphql, Link } from "gatsby";
 import Img from "gatsby-image";
-import Layout from "../components/Layout";
+import Layout from "../components/layout";
 import * as styles from "../styles/client-details.module.css";
 import {
   GenericH2,
