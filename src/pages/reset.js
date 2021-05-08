dc03c446ec94f29be12d5429fbed845d38004506
Vincent Serpoul <vincent@serpoul.com>
2021-05-08 10:35:45 +0800

    lowercase

--- src/pages/reset.js
+++ src/pages/reset.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "../components/Layout";
+import Layout from "../components/layout";
 import EngagementTiles from "../components/EngagementTiles";
 import About from "../components/About";
 import CTAGradient from "../components/CTAGradient";
