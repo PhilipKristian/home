c15c1dece6a5d697f4932e915318fbd8b24a9602
Jule Hucke <jule@eujurika.de>
2021-05-10 19:11:27 +0200

    Layouts

--- src/pages/economic-times.js
+++ src/pages/economic-times.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import { Link } from "gatsby";
 import EngagementTiles from "../components/EngagementTiles";
 import About from "../components/About";
